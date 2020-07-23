f30cd8f3d03d5d161685a39a0a1caeb7aa36f116
Tobias Klauser <tklauser@distanz.ch>
2020-07-23 18:00:29 +0200

    Update copyright
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- collector/collector.go
+++ collector/collector.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Tobias Klauser
+// Copyright 2020 Isovalent, Inc
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
