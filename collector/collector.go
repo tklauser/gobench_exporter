96e967a560338ca5c40bee2d3510e0c6dfb54d04
Tobias Klauser <tklauser@distanz.ch>
2020-07-29 10:56:30 +0200

    collector: export benchmark metrics
    Using a fixed benchmark output for now as a proof-of-concept.
    
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- collector/collector.go
+++ collector/collector.go
@@ -26,42 +26,81 @@ const namespace = "gobench"
 
 // GoBenchCollector implements the prometheus.GoBenchCollector interface.
 type GoBenchCollector struct {
-	benchmarks     bench.Set
-	benchmarksDesc *prometheus.Desc
+	benchmarks         bench.Set
+	benchmarkNamesDesc *prometheus.Desc
+	benchmarkDescs     map[string]*prometheus.Desc
+}
+
+var qtys = []string{"N", "ns/op", "B/op", "allocs/op", "MB/s"}
+
+func validPrometheusMetricName(r rune) rune {
+	// see https://github.com/prometheus/common/blob/546f1fd8d7df61d94633b254641f9f8f48248ada/model/metric.go#L92
+	switch {
+	case 'a' <= r && r <= 'z',
+		'A' <= r && r <= 'Z',
+		'0' <= r && r <= '9',
+		r == '_', r == ':':
+		return r
+	default:
+		return '_'
+	}
 }
 
 // NewGoBenchCollector
 func NewGoBenchCollector() *GoBenchCollector {
-	// for now just return static benchmarks
+	// FIXME: for now just return static benchmarks
 	in := `
 	BenchmarkSortSlice-8   	   16315	     68857 ns/op
 	PASS: main_test.go:48: MySuite.BenchmarkSortSlice	   20000	     81293 ns/op	      64 B/op	       2 allocs/op
 	`
 
 	c := &GoBenchCollector{}
-	b, err := bench.ParseSet(strings.NewReader(in))
+	bs, err := bench.ParseSet(strings.NewReader(in))
 	if err == nil {
-		c.benchmarks = b
-		c.benchmarksDesc = prometheus.NewDesc(
-			prometheus.BuildFQName(namespace, "", "go_benchmarks"),
+		c.benchmarks = bs
+		c.benchmarkNamesDesc = prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "benchmarks"),
 			"The set of Go benchmarks",
 			[]string{"name"},
 			nil,
 		)
+		c.benchmarkDescs = make(map[string]*prometheus.Desc, len(bs))
+		for _, bb := range bs {
+			for _, b := range bb {
+				name := strings.Map(validPrometheusMetricName, b.Name)
+				for _, qty := range qtys {
+					key := b.Name + qty
+					if _, ok := c.benchmarkDescs[key]; !ok {
+						c.benchmarkDescs[key] = prometheus.NewDesc(
+							prometheus.BuildFQName(namespace, "", name+strings.ReplaceAll(qty, "/", "_per_")),
+							b.Name+" "+qty,
+							nil,
+							nil,
+						)
+					}
+				}
+			}
+		}
 	}
 	return c
 }
 
 // Describe implements prometheus.Collector interface.
 func (e *GoBenchCollector) Describe(ch chan<- *prometheus.Desc) {
-	ch <- e.benchmarksDesc
+	ch <- e.benchmarkNamesDesc
 }
 
 // Collect implements prometheus.Collector interface and sends all metrics.
 func (e *GoBenchCollector) Collect(ch chan<- prometheus.Metric) {
 	for _, bb := range e.benchmarks {
 		for _, b := range bb {
-			ch <- prometheus.MustNewConstMetric(e.benchmarksDesc, prometheus.GaugeValue, 1, b.Name)
+			ch <- prometheus.MustNewConstMetric(e.benchmarkNamesDesc, prometheus.GaugeValue, 1, b.Name)
+
+			ch <- prometheus.MustNewConstMetric(e.benchmarkDescs[b.Name+qtys[0]], prometheus.GaugeValue, float64(b.N))
+			ch <- prometheus.MustNewConstMetric(e.benchmarkDescs[b.Name+qtys[1]], prometheus.GaugeValue, b.NsPerOp)
+			ch <- prometheus.MustNewConstMetric(e.benchmarkDescs[b.Name+qtys[2]], prometheus.GaugeValue, float64(b.AllocedBytesPerOp))
+			ch <- prometheus.MustNewConstMetric(e.benchmarkDescs[b.Name+qtys[3]], prometheus.GaugeValue, float64(b.AllocsPerOp))
+			ch <- prometheus.MustNewConstMetric(e.benchmarkDescs[b.Name+qtys[4]], prometheus.GaugeValue, b.MBPerS)
 		}
 	}
 }
